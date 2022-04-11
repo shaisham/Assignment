e0e76f15f4f597407311c647825c390095dacdcb
shaisham <102400953+shaisham@users.noreply.github.com>
2022-04-11 13:01:06 +0530

    Add files via upload

---
+++ C# Assignments/Assignment 1/Assignment 1/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Circle
+    {
+
+        static void area()
+        {
+            //declaring variables
+            double r, perimeter;
+
+            //Pi value 
+            double PI = 3.14;
+
+            Console.WriteLine("Radius of the circle");
+
+            // convert the string value to double 
+            r = Convert.ToDouble(Console.ReadLine());
+
+            //perimeter formula 
+            perimeter = (2 * PI * r);
+
+            Console.WriteLine("perimeter of circle is : " + perimeter);
+            // area of circle formula 
+            double area = (PI * r * r);
+
+            Console.WriteLine("Area of circle :" + area);
+        }
+        public static void Main(String[] args)
+        {
+            // calling  the method from main method 
+            area();
+        }
+    }
+}
+
+  
