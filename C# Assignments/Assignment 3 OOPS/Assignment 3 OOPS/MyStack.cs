5cbc53a6aa439f5bef4116f8878bb67ef5b3fbda
shaisham <shameershamee0@gmail.com>
2022-04-12 11:34:10 +0530

    Assignment 3 files

---
+++ C# Assignments/Assignment 3 OOPS/Assignment 3 OOPS/MyStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_OOPS
+{
+    internal class MyStack
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elemnts in stack :" + "\n");
+            Stack<int> st = new Stack<int>();
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+            st.Push(4);
+            st.Push(5);
+            foreach (var item in st)
+            {
+                Console.WriteLine(item + "\n");
+            }
+            int PopName = st.Pop();
+            
+          
+            Console.Write("the popout int is : " + PopName);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
