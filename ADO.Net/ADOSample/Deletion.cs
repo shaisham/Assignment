989f3db319e116cb8f8d6b371a100deb5bbde23e
shaisham <shameershamee0@gmail.com>
2022-04-24 13:20:37 +0530

    Added new folder called ADO.Net

---
+++ ADO.Net/ADOSample/Deletion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AdoSample
+{
+    internal class Deletion
+    {
+        static void Main(string[] args)
+        {
+            Deletion.Connection();
+            Console.ReadKey();
+        }
+
+        static void Connection()
+        {
+            string cs = "Data Source=KINGEXPT\\SQLEXPRESS;Initial Catalog=customers;Integrated Security=true;";
+            //string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+            // SqlConnection con = new SqlConnection(cs);
+            SqlConnection con = null;
+
+            try
+
+            {
+                using (con = new SqlConnection(cs))
+                {
+                    Console.WriteLine("employee ID:");
+                    string ID = Console.ReadLine();
+
+                    // deletion
+                    string query = "delete from Employee2 where id= @ID";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@ID", ID);
+
+                    con.Open();
+                    var add = cmd.ExecuteNonQuery();
+                    if (add > 0)
+                    {
+                        Console.WriteLine("data deleted successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("data not deleted");
+                    }
+
+                }
+
+            }
+            catch (SqlException ex)
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
+
+
+
+//to check connection established or not in try
+//con.Open();
+//if (con.State == ConnectionState.Open)
+//{
+//    Console.WriteLine("connection created successfully");
+//}
---
+++ ADO.Net/ADOSample/Insertion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoSample
+{
+    internal class Insertion
+    {
+        static void Main(string[] args)
+        {
+            Insertion.Connection();
+            Console.ReadKey();
+        }
+
+        static void Connection()
+        {
+            string cs = "Data Source=KINGEXPT\\SQLEXPRESS;Initial Catalog=customers;Integrated Security=true;";
+            //string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+            // SqlConnection con = new SqlConnection(cs);
+            SqlConnection con = null;
+
+            try
+
+            {
+                using (con = new SqlConnection(cs))
+                {
+                    Console.WriteLine("employee ID:");
+                    string ID = Console.ReadLine();
+                    Console.WriteLine("employee FirstName:");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("employee LastName:");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("employee Salary:");
+                    string SALARY = Console.ReadLine();
+                    Console.WriteLine("employee Address:");
+                    string Address = Console.ReadLine();
+                    Console.WriteLine("employee City:");
+                    string city = Console.ReadLine();
+
+
+
+                    string query = "insert into Employee2 values(@ID,@FirstName,@LastName,@SALARY,@Address,@City)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@ID", ID);
+                    cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", LastName);
+                    cmd.Parameters.AddWithValue("@SALARY", SALARY);
+                    cmd.Parameters.AddWithValue("@Address", Address);
+                    cmd.Parameters.AddWithValue("@City", city);
+                    con.Open();
+                    var add = cmd.ExecuteNonQuery();
+                    if (add > 0)
+                    {
+                        Console.WriteLine("data inserted successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("data not inserted");
+                    }
+
+                }
+
+            }
+            catch (SqlException ex)
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
