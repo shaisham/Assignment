318151f668e3a7735e01d35b819e761f904d411e
shaisham <shameershamee0@gmail.com>
2022-05-11 11:21:14 +0530

    Added EntityFramework Core using Code First Approach

---
+++ EFCoreCodeFirstApproach/EFCoreCodeFirstApproach/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreCodeFirstApproach.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ EFCoreCodeFirstApproach/EFCoreCodeFirstApproach/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCodeFirstApproach.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
