4cfa4f5c5be6825e9d52307eaf53c40a020f8e38
shaisham <shameershamee0@gmail.com>
2022-05-25 12:03:23 +0530

    core with swagger

---
+++ CrudEFCoreWithSwagger/CrudEFCoreWithSwagger/Context/CRUDContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudEFCoreWithSwagger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEFCoreWithSwagger.Context
+{
+    public class CRUDContext: DbContext
+    {
+
+        public CRUDContext(DbContextOptions<CRUDContext>options): base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
