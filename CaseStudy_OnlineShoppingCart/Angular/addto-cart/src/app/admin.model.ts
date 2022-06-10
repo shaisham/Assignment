721aa480504fb2656c998c99e072ebdfdd2e1e6e
shaisham <102400953+shaisham@users.noreply.github.com>
2022-06-10 12:18:24 +0530

    Delete CaseStudy_OnlineShoppingCart directory

--- CaseStudy_OnlineShoppingCart/Angular/addto-cart/src/app/admin.model.ts
+++
@@ -1,8 +0,0 @@
-export class AdminModel{
-    productId: number = 0;
-    productName: string = '';
-    productImageLink: string = '';
-    productDescription: string = '';
-    productPrice: number = 0;
-    productCategory: string = '';
-}
